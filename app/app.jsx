76b9af278123ab5a9b10b5d78caa83d637ef344f
jyy <jyy@dhcp-v880-15-00768.1Xwireless.unc.edu>
2017-12-01 10:28:31 -0500

    Heroku fix

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Provider} = require('react-redux')
+
 var{Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 
